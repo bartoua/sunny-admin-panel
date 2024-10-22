dbe9fa374650d11e7c4d1a110215f01f4a18feeb
bartoua <bartoua@gmail.com>
2024-10-23 00:58:49 +0200

    yay

--- players.php
+++ players.php
@@ -131,28 +131,6 @@ if ($order_by == 'Desc') {
                     <a href="detail_player.php?identifier=<?php echo $row['identifier']; ?>" class="btn btn-primary"><i class="glyphicon glyphicon-edit"></i></a>
                 </td>
             </tr>
-            <!-- Delete Confirmation Modal -->
-            <div class="modal fade" id="confirm-delete-<?php echo $row['id']; ?>" role="dialog">
-                <div class="modal-dialog">
-                    <form action="delete_customer.php" method="POST">
-                        <!-- Modal content -->
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal">&times;</button>
-                                <h4 class="modal-title">Confirm</h4>
-                            </div>
-                            <div class="modal-body">
-                                <input type="hidden" name="del_id" id="del_id" value="<?php echo $row['id']; ?>">
-                                <p>Are you sure you want to delete this row?</p>
-                            </div>
-                            <div class="modal-footer">
-                                <button type="submit" class="btn btn-default pull-left">Yes</button>
-                                <button type="button" class="btn btn-default" data-dismiss="modal">No</button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
             <!-- //Delete Confirmation Modal -->
             <?php endforeach;?>
         </tbody>
@@ -161,7 +139,7 @@ if ($order_by == 'Desc') {
 
     <!-- Pagination -->
     <div class="text-center">
-    <?php echo paginationLinks($page, $total_pages, 'customers.php'); ?>
+    <?php echo paginationLinks($page, $total_pages, 'players.php'); ?>
     </div>
     <!-- //Pagination -->
 </div>
