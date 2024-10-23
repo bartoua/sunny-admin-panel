1786c5e9ec99af4fa60362130c28c8853a7c994b
bartoua <bartoua@gmail.com>
2024-10-24 00:03:39 +0200

    yay

--- detail_job.php
+++ detail_job.php
@@ -13,6 +13,13 @@ if (!empty($job) and !str_starts_with($job, "sunnygroupe")) {
     $db->orderBy("u.job_grade","desc");
     $employes = $db->arraybuilder()->paginate('job_grades j', 1, $selectjob);
 
+    $selectjob = array("u.identifier", "u.firstname", "u.lastname", "j.label", "u.lastconnexion", "u.job2");
+    $db->join("users u", "u.job_grade=j.grade");
+    $db->joinWhere("users u", "u.job2 = j.job_name");
+    $db->where("u.job2", $job);
+    $db->orderBy("u.job_grade","desc");
+    $employes2 = $db->arraybuilder()->paginate('job_grades j', 1, $selectjob);
+
     // Set pagination limit
     $db->pageLimit = 30;
 
@@ -42,6 +49,32 @@ if (!empty($job) and !str_starts_with($job, "sunnygroupe")) {
         </thead>
         <tbody>
         <?php foreach ($employes as $employe) { ?>
+            <tr>
+
+                <td><?php echo xss_clean($employe["identifier"]); ?></td>
+                <td><?php echo xss_clean($employe["firstname"]); ?></td>
+                <td><?php echo xss_clean($employe["lastname"]); ?></td>
+                <td><?php echo xss_clean($employe["label"]); ?></td>
+                <td><?php echo xss_clean($employe["lastconnexion"]); ?></td>
+            </tr>
+        <?php } ?>
+        <!--<pre><?php var_dump($employes);?></pre>-->
+        </tbody>
+    </table>
+
+    <h2>Job secondaire</h2>
+    <table class="table table-striped table-bordered table-condensed">
+        <thead>
+        <tr>
+            <th width="5%">License</th>
+            <th width="5%">Prénom</th>
+            <th width="5%">Nom de famille</th>
+            <th width="5%">Grade</th>
+            <th width="5%">Dernière connexion</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($employes2 as $employe) { ?>
             <tr>
 
                 <td><?php echo xss_clean($employe["identifier"]); ?></td>
